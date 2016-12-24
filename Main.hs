b68e9d9d210ccde48b65830476d34a9f56750bd4
Patrick Pelletier <code@funwithsoftware.org>
2016-12-24 10:35:52 -0800

    split Open Pixel Control protocol into a separate file

--- Main.hs
+++ Main.hs
@@ -10,62 +10,7 @@ import Network.Socket.ByteString
 import System.Environment
 import System.Exit
 
-data Frame = SetColors
-             { fChannel :: !Word8
-             , fPixels  :: [Pixel]
-             }
-           | UnknownCommand
-             { fChannel :: !Word8
-             , fCommand :: !Word8
-             , fData    :: [Word8]
-             }
-
-data Pixel =
-  Pixel
-  { pRed   :: !Word8
-  , pGreen :: !Word8
-  , pBlue  :: !Word8
-  }
-
-cmdSetColors :: Word8
-cmdSetColors = 0
-
-instance Binary Frame where
-  put f@(SetColors {}) = do
-    putWord8 (fChannel f)
-    putWord8 cmdSetColors
-    let pix = fPixels f
-    putWord16be $ fromIntegral (3 * length pix)
-    forM_ pix put
-
-  put f@(UnknownCommand {}) = do
-    putWord8 (fChannel f)
-    putWord8 (fCommand f)
-    let d = fData f
-    putWord16be $ fromIntegral (length d)
-    forM_ d putWord8
-
-  get = do
-    ch <- getWord8
-    cmd <- getWord8
-    len <- getWord16be
-    case cmd of
-      0 {- cmdSetColors -} -> do
-        let (nPix, extra) = len `divMod` 3
-        pix <- replicateM (fromIntegral nPix) get
-        skip (fromIntegral extra)
-        return $ SetColors ch pix
-      _ -> do
-        d <- replicateM (fromIntegral len) getWord8
-        return $ UnknownCommand ch cmd d
-
-instance Binary Pixel where
-  put p = do
-    putWord8 (pRed p)
-    putWord8 (pGreen p)
-    putWord8 (pBlue p)
-
-  get = Pixel <$> getWord8 <*> getWord8 <*> getWord8
+import Opc
 
 nLights = 512                   -- max number of lights on a FadeCandy
 
